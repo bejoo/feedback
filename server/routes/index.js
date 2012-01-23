6c0a805d087a13c6253548c8f9663956a468b06c
Joris Morger <jorismorger@gmail.com>
2012-01-22 22:38:37 -0700

    must have missed something^

--- server/routes/index.js
+++ server/routes/index.js
@@ -9,8 +9,8 @@ var Feedback = require('../models/Feedback');
 exports.index = function(req, res)
 {
 //	Feedback.find({}, [], {'group': 'message'}, function (err, feedback) 
+	
 	var paginationObj = Feedback.getPagination(req.params.page);
-	console.log(paginationObj);
 	Feedback.find({}).sort('_id', 1).skip(to - 9).limit(to).execFind( function (err, feedback) 
   	{
   	  res.render('index', 
